2f790fbf9970e5bf7627c8469bd39ff053cdf408
ydtitov <ydtitov@IT-TITOV>
2016-06-21 10:55:52 +0300

    Add project files.

---
+++ Ishop2db/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using sfdb;
+using System.IO;
+
+namespace Ishop2db
+{
+    class Program
+    {
+        static string[] files = Directory.GetFiles(@"\\s-ftp\samson-f\");
+        static List<string> sqlinsert = new List<string>();
+        static int count = files.Length;
+        static int i = 0;
+        static int threadcount = 0;
+        static string[] FilePositions = new string[files.Length];
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(DateTime.Now + " - программа запущена");
+            while (i < count)
+            {
+                Console.WriteLine("Обрабатывается файл " + files[i]);
+                Thread thread = new Thread(new ParameterizedThreadStart(SetIShop));
+                thread.Start(files[i]);
+              threadcount++;
+                i++;
+            }
+            
+          //  Console.ReadKey();
+            
+        }
+
+        static void SetIShop(object filepath)
+        {
+            string file = (string)filepath;
+            IShop sf = new IShop();
+         /*   sf.FBConnectionStr = "User ID=sysdba;Password=masterkey;" +
+               "Database=server/3052:e:\\iadb\\IAPTEKA.fdb; " +
+               "DataSource=server;Charset=NONE;";*/
+            sf.LogPath = @".\app.log";
+            if (sf.ParseXmlBody(file, "ishop"))
+            {
+                Console.WriteLine("Получение даты файла " + file);
+                string datetime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                string filedate = File.GetLastWriteTime(file).ToString("yyyy-MM-dd HH:mm:ss");
+                Console.WriteLine("ЗАпись в бд дат");
+                sf.ConnectMS("UPDATE [pharmacyterminal].[dbo].[ishopinfo] " +
+                    "SET [updatedate]=Convert(datetime,'" + datetime + "', 121),"+
+                    "[filedate]=Convert(datetime,'" + filedate + "',121),"+
+                    "[fileupdate]=1 where [ishop]='" + file.Split('\\').Last() + "'");
+                Console.WriteLine("Удаление инфы о файле" + file);
+                while (sf.SelectSQLMS("SELECT * from [pharmacyterminal].[dbo].[productremains] where apt_id='" + file.Split('\\').Last() + "'") == 1)
+                {
+                    sf.ConnectMS("DELETE from [pharmacyterminal].[dbo].[productremains] where apt_id='" + file.Split('\\').Last() + "'");
+                }
+                string sql;
+                Console.WriteLine("запись в базу" + file);
+                foreach (string[] item in sf.Items)
+                {
+                    sql = "INSERT INTO [pharmacyterminal].[dbo].[productremains] ([position] ,[code] ,[name_product] ,[manufacturer] ,[country] ,[type] ,[options1001] ,[MNN] ,[price] ,[discount] ,[pre_order] ,[apt_id])" +
+                              "VALUES";
+                    sql += "(" + Convert.ToInt16(item[0]) + "," +
+                                         Convert.ToInt32(item[1]) + ",'" +
+                                         item[2].Replace('\'', ' ') + "','" +
+                                         item[3].Replace('\'', ' ') + "','" +
+                                         item[4].Replace('\'', ' ') + "','" +
+                                         item[5].Replace('\'', ' ') + "','" +
+                                         item[6].Replace('\'', ' ') + "','" +
+                                         item[7].Replace('\'', ' ') + "'," +
+                                         item[8].Replace(',', '.') + "," +
+                                         item[9].Replace(',', '.') + "," +
+                                         item[10] + ",'" + file.Split('\\').Last() + "')";
+                    sf.ConnectMS(sql);
+                 
+                }
+                sf.ConnectMS("UPDATE [pharmacyterminal].[dbo].[ishopinfo] " +
+                    "SET fileupdate=0 where ishop='" + file.Split('\\').Last() + "'");
+              
+            }
+                threadcount--;
+                if (threadcount == 0)
+                {
+                    if (!File.Exists(@".\app.log")) { using (var fs = File.Create(@".\app.log")) { } 
+                 }
+               using (StreamWriter log = new StreamWriter(@".\app.log", true, Encoding.Default))
+              {
+                  log.WriteLine("#");
+                  log.WriteLine(DateTime.Now.ToString() + " | Программа запущена");
+                  log.WriteLine("#");
+                  if (sf.error == "")
+                  {
+                      log.WriteLine("Ошибок нет");
+                  }
+                  log.WriteLine(sf.error);
+                  log.WriteLine("#");
+                  log.WriteLine(DateTime.Now.ToString() + " | Программа завершена");
+                  log.WriteLine("#");
+                  log.WriteLine("#");
+              }
+                   
+                }
+        }
+        
+
+    }
+
+}
---
+++ sfdb/SFIA.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using FirebirdSql.Data.FirebirdClient;
+using System.IO;
+using System.Xml;
+
+namespace sfdb
+{
+    public class SFIA
+    {
+        public string FBConnectionStr { get; set; }
+        public string MSConnectionStr {get; set;}
+        public string LogPath { get; set; }
+        public bool adate { get; set; }
+        public string error = "";
+
+        public SFIA()
+        {
+            FBConnectionStr = "User ID=sysdba;Password=masterkey;" +
+               "Database=server/3052:e:\\iadb\\IAPTEKA.fdb; " +
+               "DataSource=server;Charset=NONE;";
+            LogPath = @".\app.log";
+            adate = true;
+            MSConnectionStr = "data source=DBSRV01\\SQL01;persist security info=True;" +
+                "user id=infoterminal;password=9FPqY5ofy2Y8;"+
+                "MultipleActiveResultSets=True;Connection Timeout=60";
+        }
+        /// <summary>
+        /// Запрос к БД Firebird
+        /// </summary>
+        /// <param name="sql">Текст запроса</param>
+        /// <returns>FbDataReader</returns>
+     /*   public FbDataReader RunFBQuery(string sql)
+        {
+            string ConnectionString = FBConnectionStr;
+            FbConnection addDetailsConnection = new FbConnection(ConnectionString);
+            try
+            {
+                addDetailsConnection.Open();
+            }catch
+            {
+                WriteLog("Ошибка подключения к БД");   
+            }
+            FbCommand readCommand =
+          new FbCommand(sql, addDetailsConnection);
+            try
+            {
+                FbDataReader myreader = readCommand.ExecuteReader();
+                myreader.Close();
+                addDetailsConnection.Close();
+                return myreader;
+            }
+            catch
+            {
+                WriteLog("Ошибка выполнения запроса к БД");
+            }
+        }*/
+        public void ConnectMS(string sql)
+        {
+            SqlConnection myConnection = new SqlConnection(MSConnectionStr);
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                WriteLog(e.ToString());
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                SqlCommand myCommand = new SqlCommand(sql, myConnection);
+                myCommand.ExecuteNonQuery();
+                myConnection.Close();
+            }
+            catch (Exception e)
+            {
+                WriteLog(e.ToString());
+                Console.WriteLine(e.ToString());
+                myConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Запись лога в текстовый файл в папку с исполняемым файлов
+        /// </summary>
+        /// <param name="value">Строка для записи в лог</param>
+        public void WriteLog(string value)
+        {
+            error += error + value + "\r\n";
+            /*  if (!File.Exists(LogPath)){using (var fs = File.Create(LogPath)){}}
+             using (StreamWriter log = new StreamWriter(LogPath, true, Encoding.Default))
+              {
+                  if (adate){log.WriteLine(DateTime.Now.ToString() + " | " + value);}
+                  else{log.WriteLine(value);}
+              }*/
+
+        }
+    }
+
+    public class IShop : SFIA
+    {
+        public string[] Files { get; set; }
+        public List<string[]> Items = new List<string[]>();
+        public bool ParseXmlHeader(string path, string type)
+        {
+            return true;
+        }
+
+        public bool ParseXmlBody(string path, string type)
+        {
+            if(type=="ishop")
+            {
+                XmlDocument doc = CheckXml(path);
+                XmlNodeList list = doc.GetElementsByTagName("Позиция");
+                int count = list.Count;
+                if (count == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Битый файл - " + path);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return false;
+                }
+                else
+                {
+                    foreach (XmlNode position in list)
+                    {
+                        string[] arr = new string[11];
+                        arr[0] = position.Attributes[0].Value;
+                        arr[1] = position.ChildNodes[0].Attributes[0].Value;
+                        arr[2] = position.ChildNodes[1].Attributes[0].Value;
+                        arr[3] = position.ChildNodes[2].Attributes[0].Value;
+                        arr[4] = position.ChildNodes[3].Attributes[0].Value;
+                        arr[5] = position.ChildNodes[4].Attributes[0].Value;
+                        arr[6] = position.ChildNodes[5].Attributes[0].Value;
+                        arr[7] = position.ChildNodes[6].Attributes[0].Value;
+                        arr[8] = position.ChildNodes[7].Attributes[0].Value;
+                        arr[9] = position.ChildNodes[8].Attributes[0].Value;
+                        arr[10] = position.ChildNodes[9].Attributes[0].Value;
+                        //Console.WriteLine(path +" --- " +Items.Count);
+                        Items.Add(arr);
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int SelectSQLMS(string sql)
+        {
+            SqlConnection myConnection = new SqlConnection(MSConnectionStr);
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                WriteLog(e.ToString());
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                SqlCommand myCommand = new SqlCommand(sql, myConnection);
+                SqlDataReader reader =  myCommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    myConnection.Close();
+                    return 1;
+                    
+                }
+                else
+                {
+                    myConnection.Close();
+                    return 0;
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLog(e.ToString());
+                Console.WriteLine(e.ToString());
+                myConnection.Close();
+            }
+            return 0;
+        }
+
+        private XmlDocument CheckXml(string path)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(path);
+                return doc;
+            }
+            catch
+            {
+                WriteLog("Файл - " + path.Split('\\').Last() + " не является XML");
+                return new XmlDocument();
+            }
+            
+        }
+
+    }
+}
